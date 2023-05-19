cff24bc30e27448cb10b967b5927bd0065145716
Alex CB <ketepincho@gmail.com>
2023-05-20 01:26:09 +0200

    Boats now have random speed :P

--- enemies/boat/boat.gd
+++ enemies/boat/boat.gd
@@ -3,7 +3,7 @@ extends Enemy
 const DEFAULT_BOAT_ARMOR := 0
 const DEFAULT_BOAT_DAMAGE := 5
 const DEFAULT_BOAT_HEALTH := 1
-const DEFAULT_BOAT_SPEED := 40.0
+const DEFAULT_BOAT_SPEED_RANGE := Vector2(30.0, 40.0)
 
 
 func _ready():
@@ -11,5 +11,5 @@ func _ready():
 	armor = DEFAULT_BOAT_ARMOR
 	damage = DEFAULT_BOAT_DAMAGE
 	health = DEFAULT_BOAT_HEALTH
-	speed = DEFAULT_BOAT_SPEED
+	speed = randf_range(DEFAULT_BOAT_SPEED_RANGE.x, DEFAULT_BOAT_SPEED_RANGE.y)
 	animated_sprite.play("default")
