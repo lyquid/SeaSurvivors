8e6d5445560cd24c4530f5c68e9d5ee875dee52d
Alex CB <ketepincho@gmail.com>
2023-06-09 12:27:02 +0200

    Added anchor weapon

--- weapons/weapon_manager.gd
+++ weapons/weapon_manager.gd
@@ -2,11 +2,13 @@ class_name WeaponManager extends Node
 
 @onready var player := get_tree().root.get_node("Main/World/Player")
 # weapons
-enum { BALLISTA, CANNON }
+enum { ANCHOR, BALLISTA, CANNON }
+var anchor_scene := preload("res://weapons/anchor/anchor.tscn")
 var ballista_scene := preload("res://weapons/ballista/ballista.tscn")
 var cannon_scene := preload("res://weapons/cannon/cannon.tscn")
 
 
 func _ready() -> void:
-	player.call_deferred("add_child", ballista_scene.instantiate())
-	player.call_deferred("add_child", cannon_scene.instantiate())
+	player.call_deferred("add_child", anchor_scene.instantiate())
+#	player.call_deferred("add_child", ballista_scene.instantiate())
+#	player.call_deferred("add_child", cannon_scene.instantiate())
